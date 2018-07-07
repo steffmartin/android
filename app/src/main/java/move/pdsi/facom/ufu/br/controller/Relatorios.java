0b8738aac8c3cb513a0c919c7157635a86b29432
Gabriel Miranda Costa <gabrielmircosta@hotmail.com>
2018-07-07 11:28:05 -0300

    Criação das classes básicas

---
+++ app/src/main/java/move/pdsi/facom/ufu/br/controller/Relatorios.java
@@ -0,0 +1,7 @@
+package move.pdsi.facom.ufu.br.controller;
+
+/**
+ * Created by mirandagab on 07/07/2018.
+ */
+public class Relatorios {
+}
--- app/src/main/java/move/pdsi/facom/ufu/br/view/MainActivity.java
+++ app/src/main/java/move/pdsi/facom/ufu/br/view/MainActivity.java
@@ -1,7 +1,9 @@
-package move.pdsi.facom.ufu.br.move;
+package move.pdsi.facom.ufu.br.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import move.pdsi.facom.ufu.br.move.R;
 
 public class MainActivity extends AppCompatActivity {
 
